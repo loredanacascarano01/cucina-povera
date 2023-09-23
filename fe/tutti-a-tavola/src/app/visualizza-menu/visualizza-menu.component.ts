ced9b23984af9ab385c484a9730fe4f2aaf5bfe0
loredana.cascarano01 <loredana.cascarano01@icatt.it>
2023-09-23 17:07:32 +0200

    aggiunta  button canc menu + visual per titoli

--- fe/tutti-a-tavola/src/app/visualizza-menu/visualizza-menu.component.ts
+++ fe/tutti-a-tavola/src/app/visualizza-menu/visualizza-menu.component.ts
@@ -1,3 +1,5 @@
+// visualizza-menu.component.ts
+
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Menu } from '../models/menu.model';
@@ -10,13 +12,33 @@ import { Menu } from '../models/menu.model';
 export class VisualizzaMenuComponent implements OnInit {
 
   menus!: Menu[];
+  menuSelezionato: Menu | null = null;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.http.get<Menu[]>('http://localhost:8080/api/menu').subscribe(menu => {
-      this.menus = menu;
-      console.log(this.menus);
+    this.http.get<Menu[]>('http://localhost:8080/api/menu').subscribe(menus => {
+      this.menus = menus;
     });
+  }
+
+cancellaMenu(menu: Menu) {
+  const url = `http://localhost:8080/api/menu/${menu.idMenu}`;
+
+  this.http.delete(url).subscribe(() => {
+    // Rimuovi il menu dall'array dei menu
+    this.menus = this.menus.filter(m => m.idMenu !== menu.idMenu);
+  });
+}
+
+  mostraDettagli(menu: Menu) {
+    // Verifica se il menu selezionato è lo stesso del menu su cui si è fatto clic
+    if (this.menuSelezionato === menu) {
+      // Se lo è, chiudi i dettagli impostando menuSelezionato a null
+      this.menuSelezionato = null;
+    } else {
+      // Altrimenti, mostra i dettagli del menu selezionato
+      this.menuSelezionato = menu;
+    }
   }
 }
